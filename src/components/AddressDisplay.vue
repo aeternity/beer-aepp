2e471898069f36d22bbd4ebf1899cf2d085e89a4
muxe <max@apeunit.com>
2018-04-16 12:40:29 +0200

    show a little more info on account info screen

--- src/components/AddressDisplay.vue
+++ src/components/AddressDisplay.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="addressDisplay">
+    <div>
+      Somebody wants to send you tokens? They need your public key or your public name.
+      You can find both on the flyer handed out to you or on this page. The easiest way is
+      to let the other person scan the following QR Code.
+    </div>
     <vue-qrcode :value="account.pub" :options="{ size: 300 }"></vue-qrcode>
+    <div>
+      They can also enter your public name <strong>{{account.name}}</strong> and make a lookup.
+      In any case the following public key should be displayed.
+    </div>
+    <ae-address v-if="account.pub" show-avatar size='short' :address="account.pub"/>
   </div>
 </template>
 
