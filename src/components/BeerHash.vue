9ccea752b79703829e42e30dbdbdb09c8aa13676
muxe <max@apeunit.com>
2018-04-17 12:18:47 +0200

    display a tuple of txhash and a signature for the bar to verify the owner

--- src/components/BeerHash.vue
+++ src/components/BeerHash.vue
@@ -4,9 +4,6 @@
       Show this to the bartender to collect your beer!
     </div>
     <div>
-      <vue-qrcode :value="beerHash" :options="{ size: 300 }"></vue-qrcode>
-    </div>
-    <div>
       <vue-qrcode :value="fullInfo" :options="{ size: 300 }"></vue-qrcode>
     </div>
   </div>
@@ -14,6 +11,8 @@
 
 <script>
 import VueQrcode from '@xkeshi/vue-qrcode'
+// const { AeternityClient, Crypto } = require('@aeternity/aepp-sdk')
+import { Crypto } from '@aeternity/aepp-sdk'
 
 export default {
   name: 'BeerHash',
@@ -26,13 +25,16 @@ export default {
     },
     fullInfo () {
       const info = {
-        pub: this.account.pub,
-        tx: this.beerHash
+        tx: this.beerHash,
+        sig: this.beerHashSignature
       }
       return JSON.stringify(info)
     },
     beerHash () {
       return this.$route.params.beerHash
+    },
+    beerHashSignature () {
+      return this.signHash(this.beerHash, this.account.priv)
     }
   },
   data () {
@@ -40,8 +42,26 @@ export default {
     }
   },
   methods: {
+    signHash (beerHash, privateKey) {
+      const tx = beerHash
+      const sig = Crypto.sign(tx, Buffer.from(privateKey, 'hex'))
+      const sigBase64 = Buffer.from(sig).toString('base64')
+      return sigBase64
+    },
+    verifyHash (beerHash, sigBase64, pubKey) {
+      // this isnt needed here, just as a poc on how to use verify
+      const sigBuffer = Buffer.from(sigBase64, 'base64')
+      const hashBuffer = Buffer.from(beerHash)
+      const pub = Crypto.decodeBase58Check(pubKey.split('$')[1])
+      const verified = Crypto.verify(hashBuffer, sigBuffer, pub)
+      return verified
+    }
   },
   async mounted () {
+    // let sigBase64 = this.signHash(this.beerHash, this.account.priv)
+    // console.log('sigBase64', sigBase64)
+    // const verified = this.verifyHash(this.beerHash, sigBase64, this.account.pub)
+    // console.log('verified', verified)
   }
 }
 </script>
