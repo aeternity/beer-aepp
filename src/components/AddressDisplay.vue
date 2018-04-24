be951464837f0016deb8eb1adda79c97ac2c35c0
SamLife91 <sbarguti@quanterall.com>
2018-04-25 00:38:49 +0300

    Recieve Token component  - adjusting layout

--- src/components/AddressDisplay.vue
+++ src/components/AddressDisplay.vue
@@ -1,16 +1,20 @@
 <template>
-  <div class="addressDisplay">
-    <div>
+  <div class="addressDisplay shell">
+    <!-- <div>
       Somebody wants to send you tokens? They need your public key or your public name.
       You can find both on the flyer handed out to you or on this page. The easiest way is
       to let the other person scan the following QR Code.
-    </div>
+    </div> -->
+    <h1>Receive Tokens</h1>
+    <p>
+      Somebody who likes you that much, and wants to send you tokens? Show your QR Code! 
+    </p>
     <vue-qrcode :value="account.pub" :options="{ size: 300 }"></vue-qrcode>
-    <div>
+    <!-- <div>
       They can also enter your public name <strong>{{account.name}}</strong> and make a lookup.
       In any case the following public key should be displayed.
-    </div>
-    <ae-address v-if="account.pub" show-avatar size='short' :address="account.pub"/>
+    </div> -->
+    <!-- <ae-address v-if="account.pub" show-avatar size='short' :address="account.pub"/> -->
   </div>
 </template>
 
@@ -44,4 +48,12 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.addressDisplay {
+  text-align: left;
+}
+.addressDisplay canvas {
+  width: 85%;
+  display: flex;
+  margin: 0 auto;
+}
 </style>
