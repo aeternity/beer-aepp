f72a83a1aca9c5aace2bd1747f13f7420faf6cdb
SamLife91 <sbarguti@quanterall.com>
2018-04-25 00:38:01 +0300

    More information component - basic markup

---
+++ src/components/More.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="more">
+    <h1>
+      more info
+    </h1>
+    <p>
+      publick key
+    </p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'More',
+  data () {
+    return {
+
+    }
+  }
+}  
+</script>
+<style lang="scss" scoped>
+
+</style>
