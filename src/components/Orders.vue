b669218eb79656eb5897bec7dc3ad68a3560b499
SamLife91 <sbarguti@quanterall.com>
2018-04-27 12:11:27 +0300

    orders layout

--- src/components/Orders.vue
+++ src/components/Orders.vue
@@ -1,5 +1,32 @@
 <template>
-  <div class="orders"></div>
+  <div class="orders">
+    <div class="orders-top shell">
+      <h1 class="screen-title">
+        My Orders
+      </h1>
+      <p class="screen-subtitle">
+        Here is a list of all the beer you order until now.  Click an order to display the QR Code, which you can show us at the aeternity stand
+      </p>
+    </div>
+    <div class="orders-bottom">
+      <div class="orders-head">
+        <div class="orders-head-item">
+          Amount
+        </div>
+        <div class="orders-head-item">
+          Picked up?
+        </div>
+        <div class="orders-head-item">
+          
+        </div>
+      </div>
+      <div class="order-row">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
