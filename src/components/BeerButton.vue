608a6c4b6bb2616185e2f5376460d6408e765373
SamLife91 <sbarguti@quanterall.com>
2018-04-30 15:46:04 +0300

    BeerButton offsets and letter-spacing modal window

--- src/components/BeerButton.vue
+++ src/components/BeerButton.vue
@@ -174,9 +174,11 @@
         Show this QR at the æternity stand & pick up your beer(s)
       </p>
       <BeerHash :bHash='txHash'></BeerHash>
-      <ae-button type='booring'>How to get there</ae-button>
+      <router-link to='buy'>
+        <ae-button type='booring'>Close</ae-button>
+      </router-link>
     </div>
-    <div class="link" v-if="!hasTokensForBeer">
+    <div class="link" v-if="!hasTokensForBeer && ajaxCall.status !=='ready'">
       <h1 class="screen-title">
         🤔
       </h1>
@@ -515,6 +517,9 @@ input#tokensCount {
 .link .screen-title,
 .link .screen-subtitle {
   text-align:center;
+}
+.equal-icon {
+  margin: -1vh;
 }
 @keyframes swell {
   0%, 100% {
