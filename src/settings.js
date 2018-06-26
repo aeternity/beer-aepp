f439f3d58a97a663e395468d35c69ea7f94a1218
Enrico Icardi <enrico.icardi@gmail.com>
2018-06-26 17:04:30 +0200

    Use external enpoint for balance

--- src/settings.js
+++ src/settings.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line
 const SETTINGS = {
   pos_public_key: 'ak$3TRJBCvcvaegewQkexWVQkt7bEFf1tCvhvj6jfErZQNWyJ4NoyxUwkGrVVWDefxPpPEiY534fTutPaURn72HrGKCYaNWPM',
-  pos_url: 'https://republica-pos.aepps.com',
-  epoch_url: 'republica.aepps.com'
+  pos_url: 'https://pos.beer.aepps.com',
+  epoch_url: 'epoch.beer.aepps.com'
 }
